1e3b9fb2f20e300a93f6039134db6617d8fa1e52
ritheshpoojary11 <97353251+ritheshpoojary11@users.noreply.github.com>
2024-03-18 23:19:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -377,9 +377,12 @@ app.post('/updateData', async (req, res) => {
     res.status(500).json({ error: 'Error updating data' });
   }
 });
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const server = app.listen(() => {
+  console.log(`Server is running on port ${server.address().port}`);
 });
+
